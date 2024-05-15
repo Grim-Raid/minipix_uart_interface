69e8842f275092abf611c6381a1b83b22446aa83
Gregory Field <gf.field@cox.net>
2024-05-15 12:27:56 -0500

    Update start.sh

--- software/tmux/all_linux/start.sh
+++ software/tmux/all_linux/start.sh
@@ -7,10 +7,10 @@ SCRIPTPATH=`dirname $SCRIPT`
 cd "$SCRIPTPATH"
 
 # remove the old link
-rm .tmuxinator.yml
+rm .tmux.yml
 
 # link the session file to .tmuxinator.yml
-ln session.yml .tmuxinator.yml
+ln session.yml .tmux.yml
 
 # start tmuxinator
 tmux
