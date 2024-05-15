8c151670d474688c9b2423cc304cdcb725162b28
Gregory Field <gf.field@cox.net>
2024-05-15 12:13:22 -0500

    trying to fix branch

--- software/dummy/linux/include/serial_port.h
+++ software/dummy/linux/include/serial_port.h
@@ -9,6 +9,7 @@
 #include <termios.h>  // POSIX terminal control definitions
 #include <sys/ioctl.h>
 #include <string>
+#include <csdtdint>
 
 /* #include <boost/thread.hpp> */
 /* #include <boost/function.hpp> */
